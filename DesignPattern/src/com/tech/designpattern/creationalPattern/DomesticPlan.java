0d84ba17a9f58ab33c2dea72e57ce7576d16c858
GobinathanBalraj <138091426+Gobi0301@users.noreply.github.com>
2024-11-08 14:00:04 +0530

    Add files via upload

---
+++ DesignPattern/src/com/tech/designpattern/creationalPattern/DomesticPlan.java
@@ -0,0 +1,11 @@
+package com.tech.designpattern.creationalPattern;
+
+public class DomesticPlan extends Plan{
+
+	@Override
+	void getRate() {
+		// TODO Auto-generated method stub
+		rate = 3.50;
+	}
+
+}
---
+++ DesignPattern/src/com/tech/designpattern/creationalPattern/GetPlanFactory.java
@@ -0,0 +1,22 @@
+package com.tech.designpattern.creationalPattern;
+
+public class GetPlanFactory {
+
+	public Plan getPlan(String planType) {
+		if(planType == null) {
+			return null;
+		}
+		
+		if(planType.equalsIgnoreCase("COMMERCIALPLAN")) {
+			return new CommercialPlan();
+		}
+		else if(planType.equalsIgnoreCase("DOMESTICPLAN")) {
+			return new DomesticPlan();
+		}
+		else if(planType.equalsIgnoreCase("INSTITUTIONALPLAN")) {
+			return new InstitutionalPlan();
+		}
+		
+		return null;
+	}
+}
